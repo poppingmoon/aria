eda5f8299905a900042280209b4ad0050267e263
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-04-02 22:40:22 +0000

    feat: show user avatars on confirm (#31)
    * feat: show followee and follower avatar on confirm follow
    
    * feat: show account and author avatar on confirm react

---
+++ lib/view/dialog/reaction_confirmation_dialog.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:misskey_dart/misskey_dart.dart';
+
+import '../../i18n/strings.g.dart';
+import '../../model/account.dart';
+import '../../provider/api/i_notifier_provider.dart';
+import '../widget/emoji_widget.dart';
+import '../widget/user_avatar.dart';
+
+Future<bool> confirmReaction(
+  BuildContext context, {
+  required Account account,
+  required String emoji,
+  required Note note,
+}) async {
+  final result = await showDialog<bool>(
+    context: context,
+    builder: (context) => ReactionConfirmationDialog(
+      account: account,
+      emoji: emoji,
+      note: note,
+    ),
+  );
+  return result ?? false;
+}
+
+class ReactionConfirmationDialog extends ConsumerWidget {
+  const ReactionConfirmationDialog({
+    super.key,
+    required this.account,
+    required this.emoji,
+    required this.note,
+  });
+
+  final Account account;
+  final String emoji;
+  final Note note;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final i = ref.watch(iNotifierProvider(account)).valueOrNull;
+
+    return AlertDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (i != null)
+            UserAvatar(
+              user: i,
+              size: 48.0,
+              onTap: () => context.push('/$account/users/${i.id}'),
+            ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.arrow_forward, size: 36.0),
+          ),
+          UserAvatar(
+            user: note.user,
+            size: 48.0,
+            onTap: () => context.push('/$account/users/${note.userId}'),
+          ),
+        ],
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          EmojiWidget(
+            account: account,
+            emoji: emoji,
+            style:
+                DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+          ),
+          Text(t.aria.reactionConfirm),
+        ],
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () => context.pop(true),
+          child: Text(t.misskey.ok),
+        ),
+        OutlinedButton(
+          onPressed: () => context.pop(false),
+          child: Text(t.misskey.cancel),
+        ),
+      ],
+      scrollable: true,
+    );
+  }
+}
--- lib/view/widget/follow_button.dart
+++ lib/view/widget/follow_button.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 
 import '../../i18n/strings.g.dart';
 import '../../model/account.dart';
+import '../../provider/api/i_notifier_provider.dart';
 import '../../provider/api/user_notifier_provider.dart';
 import '../../provider/general_settings_notifier_provider.dart';
 import '../../util/future_with_dialog.dart';
 import '../dialog/confirmation_dialog.dart';
+import 'user_avatar.dart';
 import 'username_widget.dart';
 
 class FollowButton extends HookConsumerWidget {
@@ -71,6 +74,8 @@ class FollowButton extends HookConsumerWidget {
         child: Text(t.misskey.unfollow),
       );
     } else {
+      final i = ref.watch(iNotifierProvider(account)).valueOrNull;
+
       return ElevatedButton(
         style: ElevatedButton.styleFrom(
           foregroundColor: Theme.of(context).colorScheme.primary,
@@ -80,6 +85,26 @@ class FollowButton extends HookConsumerWidget {
           if (ref.read(generalSettingsNotifierProvider).confirmBeforeFollow) {
             final confirmed = await confirm(
               context,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (i != null)
+                    UserAvatar(
+                      user: i,
+                      size: 48.0,
+                      onTap: () => context.push('/$account/users/${i.id}'),
+                    ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.arrow_forward, size: 36.0),
+                  ),
+                  UserAvatar(
+                    user: user,
+                    size: 48.0,
+                    onTap: () => context.push('/$account/users/$userId'),
+                  ),
+                ],
+              ),
               content: Text.rich(
                 t.aria.followConfirm(
                   name: TextSpan(
@@ -91,6 +116,8 @@ class FollowButton extends HookConsumerWidget {
                   ),
                 ),
               ),
+              okText:
+                  user.isLocked ? t.misskey.followRequest : t.misskey.follow,
             );
             if (!confirmed) return;
           }
