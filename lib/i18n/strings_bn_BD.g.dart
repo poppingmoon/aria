afe8bf179e04d7e4bbd6b871173eaa833ee353a1
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-04-09 01:39:47 +0900

    chore: remove `flat_map`
    We don't use it and it takes about 2MB in builds.

--- lib/i18n/strings_bn_BD.g.dart
+++ lib/i18n/strings_bn_BD.g.dart
@@ -18,16 +18,10 @@ class _StringsBnBd extends Translations {
 		    cardinalResolver: cardinalResolver,
 		    ordinalResolver: ordinalResolver,
 		  ),
-		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver) {
-		super.$meta.setFlatMapFunction($meta.getTranslation); // copy base translations to super.$meta
-		$meta.setFlatMapFunction(_flatMapFunction);
-	}
+		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver);
 
 	/// Metadata for the translations of <bn-BD>.
 	@override final TranslationMetadata<AppLocale, Translations> $meta;
-
-	/// Access flat map
-	@override dynamic operator[](String key) => $meta.getTranslation(key) ?? super.$meta.getTranslation(key);
 
 	@override late final _StringsBnBd _root = this; // ignore: unused_field
 
--- lib/i18n/strings_pt_PT.g.dart
+++ lib/i18n/strings_pt_PT.g.dart
@@ -18,16 +18,10 @@ class _StringsPtPt extends Translations {
 		    cardinalResolver: cardinalResolver,
 		    ordinalResolver: ordinalResolver,
 		  ),
-		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver) {
-		super.$meta.setFlatMapFunction($meta.getTranslation); // copy base translations to super.$meta
-		$meta.setFlatMapFunction(_flatMapFunction);
-	}
+		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver);
 
 	/// Metadata for the translations of <pt-PT>.
 	@override final TranslationMetadata<AppLocale, Translations> $meta;
-
-	/// Access flat map
-	@override dynamic operator[](String key) => $meta.getTranslation(key) ?? super.$meta.getTranslation(key);
 
 	@override late final _StringsPtPt _root = this; // ignore: unused_field
 
