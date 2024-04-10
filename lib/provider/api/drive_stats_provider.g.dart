f83157ad1072874dc6ea762811cface05d730e18
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-04-10 23:04:26 +0000

    feat: show drive usage (#73)
    Add a graph that displays usage and capacity of drive to
    `DriveSettingsPage`.
    Also show usage amount in `DrivePage`.

---
+++ lib/provider/api/drive_stats_provider.g.dart
@@ -0,0 +1,158 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'drive_stats_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$driveStatsHash() => r'2998813a782013c4943d007558bfb20720172b8a';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [driveStats].
+@ProviderFor(driveStats)
+const driveStatsProvider = DriveStatsFamily();
+
+/// See also [driveStats].
+class DriveStatsFamily extends Family<AsyncValue<DriveResponse>> {
+  /// See also [driveStats].
+  const DriveStatsFamily();
+
+  /// See also [driveStats].
+  DriveStatsProvider call(
+    Account account,
+  ) {
+    return DriveStatsProvider(
+      account,
+    );
+  }
+
+  @override
+  DriveStatsProvider getProviderOverride(
+    covariant DriveStatsProvider provider,
+  ) {
+    return call(
+      provider.account,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'driveStatsProvider';
+}
+
+/// See also [driveStats].
+class DriveStatsProvider extends AutoDisposeFutureProvider<DriveResponse> {
+  /// See also [driveStats].
+  DriveStatsProvider(
+    Account account,
+  ) : this._internal(
+          (ref) => driveStats(
+            ref as DriveStatsRef,
+            account,
+          ),
+          from: driveStatsProvider,
+          name: r'driveStatsProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$driveStatsHash,
+          dependencies: DriveStatsFamily._dependencies,
+          allTransitiveDependencies:
+              DriveStatsFamily._allTransitiveDependencies,
+          account: account,
+        );
+
+  DriveStatsProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.account,
+  }) : super.internal();
+
+  final Account account;
+
+  @override
+  Override overrideWith(
+    FutureOr<DriveResponse> Function(DriveStatsRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: DriveStatsProvider._internal(
+        (ref) => create(ref as DriveStatsRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        account: account,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeFutureProviderElement<DriveResponse> createElement() {
+    return _DriveStatsProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is DriveStatsProvider && other.account == account;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, account.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin DriveStatsRef on AutoDisposeFutureProviderRef<DriveResponse> {
+  /// The parameter `account` of this provider.
+  Account get account;
+}
+
+class _DriveStatsProviderElement
+    extends AutoDisposeFutureProviderElement<DriveResponse> with DriveStatsRef {
+  _DriveStatsProviderElement(super.provider);
+
+  @override
+  Account get account => (origin as DriveStatsProvider).account;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/view/page/settings/drive_settings_page.dart
+++ lib/view/page/settings/drive_settings_page.dart
@@ -1,13 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
+import 'package:pretty_bytes/pretty_bytes.dart';
 
 import '../../../i18n/strings.g.dart';
 import '../../../model/account.dart';
 import '../../../provider/account_settings_notifier_provider.dart';
 import '../../../provider/api/drive_folder_provider.dart';
+import '../../../provider/api/drive_stats_provider.dart';
 import '../../../provider/api/i_notifier_provider.dart';
 import '../../../util/future_with_dialog.dart';
+import '../../widget/key_value_widget.dart';
 import '../drive_page.dart';
 
 class DriveSettingsPage extends ConsumerWidget {
@@ -19,6 +24,12 @@ class DriveSettingsPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final settings = ref.watch(accountSettingsNotifierProvider(account));
     final i = ref.watch(iNotifierProvider(account)).valueOrNull;
+    final stats = ref.watch(driveStatsProvider(account)).valueOrNull;
+    final usageRatio = stats != null
+        ? stats.usage / max(max(1, stats.usage), stats.capacity).toDouble()
+        : 0.0;
+    final barColor =
+        HSLColor.fromAHSL(1.0, (1 - usageRatio) * 180, 0.7, 0.5).toColor();
     final uploadFolder = settings.uploadFolder != null
         ? ref
             .watch(driveFolderProvider(account, settings.uploadFolder!))
@@ -29,6 +40,61 @@ class DriveSettingsPage extends ConsumerWidget {
       appBar: AppBar(title: Text(t.misskey.drive)),
       body: ListView(
         children: [
+          if (stats != null) ...[
+            ListTile(
+              title: Text(t.misskey.usageAmount),
+              subtitle: Container(
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                height: 16.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      barColor,
+                      barColor,
+                      Colors.black12,
+                    ],
+                    stops: [
+                      0.0,
+                      usageRatio,
+                      usageRatio,
+                    ],
+                  ),
+                  border: Border.all(color: Colors.white30),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: KeyValueWidget(
+                      label: t.misskey.inUse,
+                      text: prettyBytes(
+                        stats.usage.toDouble(),
+                        locale: Localizations.localeOf(context).toLanguageTag(),
+                        binary: true,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: KeyValueWidget(
+                      label: t.misskey.capacity,
+                      text: prettyBytes(
+                        stats.capacity.toDouble(),
+                        locale: Localizations.localeOf(context).toLanguageTag(),
+                        binary: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(),
+          ],
           ListTile(
             title: Text(t.misskey.uploadFolder),
             subtitle: settings.uploadFolder == null || uploadFolder != null
