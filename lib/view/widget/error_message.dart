06021f418b9b8270aae31e9f5515731f2d878f8c
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-04-23 20:57:28 +0900

    feat: show descriptions of errors

--- lib/view/widget/error_message.dart
+++ lib/view/widget/error_message.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
+import 'dart:convert';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:misskey_dart/misskey_dart.dart';
 
 import '../../i18n/strings.g.dart';
 import '../../util/copy_text.dart';
@@ -18,24 +17,51 @@ class ErrorMessage extends HookWidget {
   final Object? error;
   final StackTrace? stackTrace;
 
-  String _getErrorMessage(Object? error) {
-    return switch (error) {
-      DioException(:final error?) => _getErrorMessage(error),
-      DioException(
-        response: Response(data: {'error': {'message': final String message}})
-      ) =>
-        message,
-      WebSocketChannelException(:final inner?) => _getErrorMessage(inner),
-      WebSocketChannelException(:final message?) => message,
-      WebSocketException(:final message) => message,
-      _ => error.toString(),
-    };
-  }
-
   @override
   Widget build(BuildContext context) {
     final showStackTrace = useState(false);
-    final message = _getErrorMessage(error);
+    final error = this.error;
+    final message = error is MisskeyException
+        ? switch (error.code) {
+            'INTERNAL_ERROR' => [
+                t.misskey.internalServerError,
+                t.misskey.internalServerErrorDescription,
+                const JsonEncoder.withIndent('  ').convert(error.info),
+              ],
+            'RATE_LIMIT_EXCEEDED' => [
+                t.misskey.cannotPerformTemporary,
+                t.misskey.cannotPerformTemporaryDescription,
+              ],
+            'INVALID_PARAM' => [
+                t.misskey.invalidParamError,
+                t.misskey.invalidParamErrorDescription,
+                if (error.info != null)
+                  const JsonEncoder.withIndent('  ').convert(error.info),
+              ],
+            'ROLE_PERMISSION_DENIED' => [
+                t.misskey.permissionDeniedError,
+                t.misskey.permissionDeniedErrorDescription,
+              ],
+            'RECURSIVE_NESTING' => [t.misskey.circularReferenceFolder],
+            'HAS_CHILD_FILES_OR_FOLDERS' => [t.misskey.hasChildFilesOrFolders],
+            'NO_SUCH_ROLE' => [t.misskey.noRole],
+            'NAME_ALREADY_EXISTS' => [t.misskey.pages_.nameAlreadyExists],
+            'NO_SUCH_USER' || 'USER_NOT_FOUND' => [t.misskey.noSuchUser],
+            _ => error.code.startsWith('TOO_MANY')
+                ? [
+                    t.misskey.youCannotCreateAnymore,
+                    error.message,
+                    error.id,
+                  ]
+                : [
+                    error.code,
+                    error.message,
+                    if (error.info != null)
+                      const JsonEncoder.withIndent('  ').convert(error.info),
+                  ]
+          }
+            .join('\n')
+        : error.toString();
 
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -51,7 +77,8 @@ class ErrorMessage extends HookWidget {
           ListTile(
             title: Text(t.aria.stackTrace),
             subtitle: InkWell(
-              onLongPress: () => copyToClipboard(context, message),
+              onLongPress: () =>
+                  copyToClipboard(context, stackTrace.toString()),
               child: Text(stackTrace.toString()),
             ),
           ),
