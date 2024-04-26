029edd5794319a285bae9aeef89d134b8a9909bb
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-04-26 12:11:11 +0900

    fix: tweak design of mfm search

--- lib/view/widget/mfm/search.dart
+++ lib/view/widget/mfm/search.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../i18n/strings.g.dart';
+import '../url_sheet.dart';
 
 class Search extends HookWidget {
   const Search({super.key, required this.query});
@@ -11,18 +12,50 @@ class Search extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController(text: query);
-
-    return TextFormField(
-      controller: controller,
-      readOnly: true,
-      decoration: InputDecoration(
-        suffix: ElevatedButton(
-          onPressed: () => launchUrl(
-            Uri.https('google.com', 'search', {'q': query}),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.outlineVariant),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SelectableText(query),
+            ),
           ),
-          child: Text(t.misskey.searchByGoogle),
-        ),
+          Material(
+            color: Colors.black12,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(4.0),
+              bottomRight: Radius.circular(4.0),
+            ),
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: () => launchUrl(
+                Uri.https('google.com', 'search', {'q': query}),
+              ),
+              onLongPress: () => showModalBottomSheet<void>(
+                context: context,
+                builder: (context) => UrlSheet(
+                  url: Uri.https('google.com', 'search', {'q': query})
+                      .toString(),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.search),
+                    Text(t.misskey.searchByGoogle),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
