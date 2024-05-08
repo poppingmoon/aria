7d1ea20b87fda8ecb2e61dff966be1870197b9a5
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-05-08 11:35:54 +0000

    fix: fold notifications on the client side (#136)
    If `useGroupedNotifications` is enabled and the server doesn't have
    `i/notifications-grouped` endpoint, create grouped notifications from
    response from `i/notifications`.

--- lib/provider/api/notifications_notifier_provider.dart
+++ lib/provider/api/notifications_notifier_provider.dart
@@ -5,6 +5,7 @@ import '../../model/account.dart';
 import '../../model/pagination_state.dart';
 import '../general_settings_notifier_provider.dart';
 import '../notes_notifier_provider.dart';
+import 'endpoints_provider.dart';
 import 'misskey_provider.dart';
 
 part 'notifications_notifier_provider.g.dart';
@@ -19,23 +20,132 @@ class NotificationsNotifier extends _$NotificationsNotifier {
     return PaginationState.fromIterable(response);
   }
 
+  List<INotificationsResponse> _foldNotifications(
+    Iterable<INotificationsResponse> notifications,
+  ) {
+    return notifications.fold(
+      [],
+      (acc, notification) => switch (notification.type) {
+        NotificationType.reaction => switch (acc.lastOrNull) {
+            final previousNotification? => switch (previousNotification.type) {
+                NotificationType.reaction => [
+                    ...acc.toList().sublist(0, acc.length - 1),
+                    if (previousNotification.note?.id == notification.note?.id)
+                      previousNotification.copyWith(
+                        id: notification.id,
+                        type: NotificationType.reactionGrouped,
+                        reaction: null,
+                        user: null,
+                        reactions: [
+                          if (previousNotification
+                              case INotificationsResponse(
+                                :final user?,
+                                :final reaction?
+                              ))
+                            INotificationsReaction(
+                              user: user,
+                              reaction: reaction,
+                            ),
+                          if (notification
+                              case INotificationsResponse(
+                                :final user?,
+                                :final reaction?
+                              ))
+                            INotificationsReaction(
+                              user: user,
+                              reaction: reaction,
+                            ),
+                        ],
+                      )
+                    else ...[previousNotification, notification],
+                  ],
+                NotificationType.reactionGrouped => [
+                    ...acc.toList().sublist(0, acc.length - 1),
+                    if (previousNotification.note?.id == notification.note?.id)
+                      previousNotification.copyWith(
+                        id: notification.id,
+                        reactions: [
+                          ...?previousNotification.reactions,
+                          if (notification
+                              case INotificationsResponse(
+                                :final user?,
+                                :final reaction?
+                              ))
+                            INotificationsReaction(
+                              user: user,
+                              reaction: reaction,
+                            ),
+                        ],
+                      )
+                    else ...[previousNotification, notification],
+                  ],
+                _ => [...acc, notification],
+              },
+            _ => [...acc, notification],
+          },
+        NotificationType.renote => switch (acc.lastOrNull) {
+            final previousNotification? => switch (previousNotification.type) {
+                NotificationType.renote => [
+                    ...acc.toList().sublist(0, acc.length - 1),
+                    if (previousNotification.note?.renoteId ==
+                        notification.note?.renoteId)
+                      previousNotification.copyWith(
+                        id: notification.id,
+                        type: NotificationType.renoteGrouped,
+                        userId: null,
+                        user: null,
+                        users: [previousNotification.user, notification.user]
+                            .nonNulls
+                            .toList(),
+                      )
+                    else ...[previousNotification, notification],
+                  ],
+                NotificationType.renoteGrouped => [
+                    ...acc.toList().sublist(0, acc.length - 1),
+                    if (previousNotification.note?.renoteId ==
+                        notification.note?.renoteId)
+                      previousNotification.copyWith(
+                        id: notification.id,
+                        users: [
+                          ...?previousNotification.users,
+                          notification.user,
+                        ].nonNulls.toList(),
+                      )
+                    else ...[previousNotification, notification],
+                  ],
+                _ => [...acc, notification],
+              },
+            _ => [...acc, notification],
+          },
+        _ => [...acc, notification],
+      },
+    );
+  }
+
   Future<Iterable<INotificationsResponse>> _fetchNotifications({
     String? untilId,
   }) async {
-    final notifications =
-        ref.read(generalSettingsNotifierProvider).useGroupedNotifications
-            ? await ref.read(misskeyProvider(account)).i.notificationsGrouped(
-                  INotificationsGroupedRequest(
-                    untilId: untilId,
-                    limit: 20,
-                  ),
-                )
-            : await ref.read(misskeyProvider(account)).i.notifications(
-                  INotificationsRequest(
-                    untilId: untilId,
-                    limit: 20,
-                  ),
+    final Iterable<INotificationsResponse> notifications;
+    if (ref.read(generalSettingsNotifierProvider).useGroupedNotifications) {
+      final endpoints = await ref.read(endpointsProvider(account.host).future);
+      if (endpoints.contains('i/notifications-grouped')) {
+        notifications =
+            await ref.read(misskeyProvider(account)).i.notificationsGrouped(
+                  INotificationsGroupedRequest(untilId: untilId, limit: 20),
                 );
+      } else {
+        final response = await ref
+            .read(misskeyProvider(account))
+            .i
+            .notifications(INotificationsRequest(untilId: untilId, limit: 20));
+        notifications = _foldNotifications(response);
+      }
+    } else {
+      notifications = await ref
+          .read(misskeyProvider(account))
+          .i
+          .notifications(INotificationsRequest(untilId: untilId, limit: 20));
+    }
     ref.read(notesNotifierProvider(account).notifier).addAll(
           notifications.map((notification) => notification.note).nonNulls,
         );
