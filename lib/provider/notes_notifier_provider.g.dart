3596ac8dc6466ceaecbf65c0d2ebb68c7cfefd1e
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-07-01 06:25:34 +0900

    fix: add renote as detailed at `NotesNotifier`
    This fixes an issue where polls contained in renotes are not visible.

--- lib/provider/notes_notifier_provider.g.dart
+++ lib/provider/notes_notifier_provider.g.dart
@@ -6,7 +6,7 @@ part of 'notes_notifier_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$notesNotifierHash() => r'8f317ab70fe8e6bfa42b4ed3150f79c4f73fa598';
+String _$notesNotifierHash() => r'968bdeff15453f941f975ac961e89154c3148a2c';
 
 /// Copied from Dart SDK
 class _SystemHash {
