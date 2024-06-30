7a9ebe2ea6cb171b15971c4a9c0f0d164d73010a
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-06-30 10:23:45 +0900

    refactor: extract `TagNotes` widget

---
+++ lib/view/page/tag/tag_notes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../i18n/strings.g.dart';
+import '../../../model/account.dart';
+import '../../../provider/api/tag_notes_notifier_provider.dart';
+import '../../widget/note_widget.dart';
+import '../../widget/paginated_list_view.dart';
+
+class TagNotes extends ConsumerWidget {
+  const TagNotes({
+    super.key,
+    required this.account,
+    required this.tag,
+  });
+
+  final Account account;
+  final String tag;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notes = ref.watch(tagNotesNotifierProvider(account, tag));
+
+    return PaginatedListView(
+      paginationState: notes,
+      itemBuilder: (context, note) =>
+          NoteWidget(account: account, noteId: note.id),
+      onRefresh: () =>
+          ref.refresh(tagNotesNotifierProvider(account, tag).future),
+      loadMore: (skipError) => ref
+          .read(tagNotesNotifierProvider(account, tag).notifier)
+          .loadMore(skipError: skipError),
+      noItemsLabel: t.misskey.noNotes,
+    );
+  }
+}
--- lib/view/page/tag/tag_page.dart
+++ lib/view/page/tag/tag_page.dart
@@ -5,9 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../i18n/strings.g.dart';
 import '../../../model/account.dart';
 import '../../../provider/api/post_notifier_provider.dart';
-import '../../../provider/api/tag_notes_notifier_provider.dart';
-import '../../widget/note_widget.dart';
-import '../../widget/paginated_list_view.dart';
+import 'tag_notes.dart';
 
 class TagPage extends ConsumerWidget {
   const TagPage({
@@ -21,21 +19,9 @@ class TagPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final notes = ref.watch(tagNotesNotifierProvider(account, tag));
-
     return Scaffold(
       appBar: AppBar(title: Text('#$tag')),
-      body: PaginatedListView(
-        paginationState: notes,
-        itemBuilder: (context, note) =>
-            NoteWidget(account: account, noteId: note.id),
-        onRefresh: () =>
-            ref.refresh(tagNotesNotifierProvider(account, tag).future),
-        loadMore: (skipError) => ref
-            .read(tagNotesNotifierProvider(account, tag).notifier)
-            .loadMore(skipError: skipError),
-        noItemsLabel: t.misskey.noNotes,
-      ),
+      body: TagNotes(account: account, tag: tag),
       floatingActionButton: account.isGuest
           ? null
           : FloatingActionButton.extended(
