a2e3d103c6ea7e4eed7fe311f39e6c678a9007b7
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-07-14 04:43:05 +0900

    fix: unawait deleting tab (#294)

--- lib/view/page/settings/tab_settings_page.dart
+++ lib/view/page/settings/tab_settings_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart' hide ImageIcon;
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -183,9 +185,11 @@ class TabSettingsPage extends HookConsumerWidget {
                         message: t.aria.deleteTabConfirm,
                       );
                       if (confirmed) {
-                        await ref
-                            .read(timelineTabsNotifierProvider.notifier)
-                            .delete(initialTabSettings.id!);
+                        unawaited(
+                          ref
+                              .read(timelineTabsNotifierProvider.notifier)
+                              .delete(initialTabSettings.id!),
+                        );
                         if (!context.mounted) return;
                         context.pop();
                       }
