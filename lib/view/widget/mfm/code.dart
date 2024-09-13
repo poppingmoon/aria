78fe2e2a7ea870d62011e31024e19ead11b24aac
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-09-13 20:33:06 +0900

    feat: add copy button on code block

--- lib/view/widget/mfm/code.dart
+++ lib/view/widget/mfm/code.dart
@@ -5,6 +5,8 @@ import 'package:flutter_highlighting/themes/atom-one-dark.dart';
 import 'package:highlighting/languages/all.dart';
 
 import '../../../constant/fonts.dart';
+import '../../../extension/text_style_extension.dart';
+import '../../../i18n/strings.g.dart';
 import '../../../util/copy_text.dart';
 
 class Code extends StatelessWidget {
@@ -14,12 +16,14 @@ class Code extends StatelessWidget {
     this.language,
     this.inline = false,
     this.fontSize,
+    this.copyOnTap = false,
   });
 
   final String code;
   final String? language;
   final bool inline;
   final double? fontSize;
+  final bool copyOnTap;
 
   @override
   Widget build(BuildContext context) {
@@ -29,31 +33,52 @@ class Code extends StatelessWidget {
       Brightness.dark => atomOneDarkTheme,
     };
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(4.0),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(4.0),
-        onLongPress: () => copyToClipboard(context, code),
-        child: Container(
-          width: inline ? null : double.infinity,
-          color: theme['root']?.backgroundColor,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: HighlightView(
-              code,
-              languageId: languageId,
-              theme: theme,
-              padding: inline
-                  ? const EdgeInsets.symmetric(horizontal: 2.0)
-                  : const EdgeInsets.all(8.0),
-              textStyle: TextStyle(
-                fontSize: fontSize,
-                fontFamilyFallback: monospaceFallback,
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(inline ? 4.0 : 8.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(inline ? 4.0 : 8.0),
+            onTap: copyOnTap ? () => copyToClipboard(context, code) : null,
+            onLongPress: () => copyToClipboard(context, code),
+            child: Container(
+              width: inline ? null : double.infinity,
+              color: theme['root']?.backgroundColor,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: HighlightView(
+                  code,
+                  languageId: languageId,
+                  theme: theme,
+                  padding: inline
+                      ? const EdgeInsets.symmetric(horizontal: 2.0)
+                      : const EdgeInsets.all(16.0),
+                  textStyle: TextStyle(
+                    fontSize: fontSize,
+                    fontFamilyFallback: monospaceFallback,
+                  ),
+                ),
               ),
             ),
           ),
         ),
-      ),
+        if (!inline)
+          Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              tooltip: t.misskey.copy,
+              style: IconButton.styleFrom(
+                foregroundColor:
+                    Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+                minimumSize: Size.zero,
+                iconSize: DefaultTextStyle.of(context).style.lineHeight,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              onPressed: () => copyToClipboard(context, code),
+              icon: const Icon(Icons.copy),
+            ),
+          ),
+      ],
     );
   }
 }
