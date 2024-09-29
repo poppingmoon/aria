317b9c59a06fc196c94e1790ca8753e250b4b429
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-09-29 13:35:16 +0900

    feat: add SwRegisterDialog
    The `sw/register` endpoint of Misskey is marked `secure` and cannot be
    accessed using an access token issued with MiAuth.
    This dialog generates an AiScript code that sends the request and
    prompts the user to run it on the scratchpad of Misskey Web.
    Once the code is executed, the scratchpad shows a dialog with the
    response.
    Finally, by pasting the response, Aria can indirectly utilise the
    endpoint.

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -4,7 +4,7 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 31
-/// Strings: 54576 (1760 per locale)
+/// Strings: 54579 (1760 per locale)
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
