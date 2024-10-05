e672e6015c1c0b05aaa81cc2a87c995b550a9238
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-10-06 06:09:34 +0900

    feat: add pagination for featured plays (#404)

---
+++ lib/provider/api/featured_plays_notifier_provider.dart
@@ -0,0 +1,48 @@
+import 'package:misskey_dart/misskey_dart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../model/account.dart';
+import '../../model/pagination_state.dart';
+import 'misskey_provider.dart';
+
+part 'featured_plays_notifier_provider.g.dart';
+
+@riverpod
+class FeaturedPlaysNotifier extends _$FeaturedPlaysNotifier {
+  @override
+  FutureOr<PaginationState<Flash>> build(Account account) async {
+    final response = await _fetchPlays();
+    return PaginationState.fromIterable(response);
+  }
+
+  Future<Iterable<Flash>> _fetchPlays({int? offset}) async {
+    return ref
+        .read(misskeyProvider(account))
+        .flash
+        .featured(FlashFeaturedRequest(offset: offset));
+  }
+
+  Future<void> loadMore({bool skipError = false}) async {
+    if (state.isLoading || (state.hasError && !skipError)) {
+      return;
+    }
+    final value = skipError ? state.valueOrNull! : await future;
+    if (value.isLastLoaded) {
+      return;
+    }
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final response = await _fetchPlays(offset: value.items.length);
+      if (response.any((play) => play.id == value.items.firstOrNull?.id)) {
+        // Pagination for `flash/featured` was not supported
+        // until Misskey 2024.10.0.
+        return value.copyWith(isLastLoaded: true);
+      } else {
+        return PaginationState(
+          items: [...value.items, ...response],
+          isLastLoaded: response.isEmpty,
+        );
+      }
+    });
+  }
+}
