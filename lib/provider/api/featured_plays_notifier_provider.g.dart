f1c5cc713be8a19fe394c6c2113ee26adeb466ae
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-10-06 05:47:20 +0900

    feat: add pagination for featured plays

---
+++ lib/provider/api/featured_plays_notifier_provider.g.dart
@@ -0,0 +1,178 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'featured_plays_notifier_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$featuredPlaysNotifierHash() =>
+    r'e417e1f3cc27616906ae33aa35c5729332acbd99';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$FeaturedPlaysNotifier
+    extends BuildlessAutoDisposeAsyncNotifier<PaginationState<Flash>> {
+  late final Account account;
+
+  FutureOr<PaginationState<Flash>> build(
+    Account account,
+  );
+}
+
+/// See also [FeaturedPlaysNotifier].
+@ProviderFor(FeaturedPlaysNotifier)
+const featuredPlaysNotifierProvider = FeaturedPlaysNotifierFamily();
+
+/// See also [FeaturedPlaysNotifier].
+class FeaturedPlaysNotifierFamily
+    extends Family<AsyncValue<PaginationState<Flash>>> {
+  /// See also [FeaturedPlaysNotifier].
+  const FeaturedPlaysNotifierFamily();
+
+  /// See also [FeaturedPlaysNotifier].
+  FeaturedPlaysNotifierProvider call(
+    Account account,
+  ) {
+    return FeaturedPlaysNotifierProvider(
+      account,
+    );
+  }
+
+  @override
+  FeaturedPlaysNotifierProvider getProviderOverride(
+    covariant FeaturedPlaysNotifierProvider provider,
+  ) {
+    return call(
+      provider.account,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'featuredPlaysNotifierProvider';
+}
+
+/// See also [FeaturedPlaysNotifier].
+class FeaturedPlaysNotifierProvider
+    extends AutoDisposeAsyncNotifierProviderImpl<FeaturedPlaysNotifier,
+        PaginationState<Flash>> {
+  /// See also [FeaturedPlaysNotifier].
+  FeaturedPlaysNotifierProvider(
+    Account account,
+  ) : this._internal(
+          () => FeaturedPlaysNotifier()..account = account,
+          from: featuredPlaysNotifierProvider,
+          name: r'featuredPlaysNotifierProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$featuredPlaysNotifierHash,
+          dependencies: FeaturedPlaysNotifierFamily._dependencies,
+          allTransitiveDependencies:
+              FeaturedPlaysNotifierFamily._allTransitiveDependencies,
+          account: account,
+        );
+
+  FeaturedPlaysNotifierProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.account,
+  }) : super.internal();
+
+  final Account account;
+
+  @override
+  FutureOr<PaginationState<Flash>> runNotifierBuild(
+    covariant FeaturedPlaysNotifier notifier,
+  ) {
+    return notifier.build(
+      account,
+    );
+  }
+
+  @override
+  Override overrideWith(FeaturedPlaysNotifier Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: FeaturedPlaysNotifierProvider._internal(
+        () => create()..account = account,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        account: account,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeAsyncNotifierProviderElement<FeaturedPlaysNotifier,
+      PaginationState<Flash>> createElement() {
+    return _FeaturedPlaysNotifierProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is FeaturedPlaysNotifierProvider && other.account == account;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, account.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin FeaturedPlaysNotifierRef
+    on AutoDisposeAsyncNotifierProviderRef<PaginationState<Flash>> {
+  /// The parameter `account` of this provider.
+  Account get account;
+}
+
+class _FeaturedPlaysNotifierProviderElement
+    extends AutoDisposeAsyncNotifierProviderElement<FeaturedPlaysNotifier,
+        PaginationState<Flash>> with FeaturedPlaysNotifierRef {
+  _FeaturedPlaysNotifierProviderElement(super.provider);
+
+  @override
+  Account get account => (origin as FeaturedPlaysNotifierProvider).account;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/view/page/play/plays_featured.dart
+++ lib/view/page/play/plays_featured.dart
@@ -4,9 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../i18n/strings.g.dart';
 import '../../../model/account.dart';
-import '../../../provider/api/featured_pages_provider.dart';
-import '../../../provider/api/featured_plays_provider.dart';
-import '../../widget/error_message.dart';
+import '../../../provider/api/featured_plays_notifier_provider.dart';
+import '../../widget/paginated_list_view.dart';
 import '../../widget/play_preview.dart';
 
 class PlaysFeatured extends ConsumerWidget {
@@ -16,63 +15,21 @@ class PlaysFeatured extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final plays = ref.watch(featuredPlaysProvider(account));
+    final plays = ref.watch(featuredPlaysNotifierProvider(account));
 
-    return RefreshIndicator(
-      onRefresh: () => ref.refresh(featuredPagesProvider(account).future),
-      child: Center(
-        child: switch (plays) {
-          AsyncValue(valueOrNull: final plays?) => plays.isEmpty
-              ? Text(t.misskey.nothing)
-              : Container(
-                  width: 800.0,
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ListTileTheme(
-                    tileColor: Theme.of(context).colorScheme.surface,
-                    child: ListView(
-                      children: [
-                        Container(
-                          height: 8.0,
-                          margin: const EdgeInsets.only(top: 8.0),
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8.0),
-                              topRight: Radius.circular(8.0),
-                            ),
-                            color: Theme.of(context).colorScheme.surface,
-                          ),
-                        ),
-                        ...ListTile.divideTiles(
-                          context: context,
-                          tiles: plays.map(
-                            (play) => PlayPreview(
-                              account: account,
-                              play: play,
-                              onTap: () =>
-                                  context.push('/$account/play/${play.id}'),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          height: 8.0,
-                          margin: const EdgeInsets.only(bottom: 8.0),
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              bottomLeft: Radius.circular(8.0),
-                              bottomRight: Radius.circular(8.0),
-                            ),
-                            color: Theme.of(context).colorScheme.surface,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-          AsyncValue(:final error?, :final stackTrace) =>
-            ErrorMessage(error: error, stackTrace: stackTrace),
-          _ => const CircularProgressIndicator(),
-        },
+    return PaginatedListView(
+      paginationState: plays,
+      itemBuilder: (context, play) => PlayPreview(
+        account: account,
+        play: play,
+        onTap: () => context.push('/$account/play/${play.id}'),
       ),
+      onRefresh: () =>
+          ref.refresh(featuredPlaysNotifierProvider(account).future),
+      loadMore: (skipError) => ref
+          .read(featuredPlaysNotifierProvider(account).notifier)
+          .loadMore(skipError: skipError),
+      noItemsLabel: t.misskey.nothing,
     );
   }
 }
