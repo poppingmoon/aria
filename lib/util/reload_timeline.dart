6b9f8c19e6c3ce3d0ee47419a5159c56164dbac5
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-10-29 00:07:45 +0900

    refactor: separate reload timeline function

---
+++ lib/util/reload_timeline.dart
@@ -0,0 +1,25 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../model/tab_settings.dart';
+import '../provider/api/timeline_notes_after_note_notifier_provider.dart';
+import '../provider/api/timeline_notes_notifier_provider.dart';
+import '../provider/streaming/timeline_stream_notifier.dart';
+import '../provider/streaming/web_socket_channel_provider.dart';
+import '../provider/timeline_center_notifier_provider.dart';
+import '../provider/timeline_last_viewed_note_id_notifier_provider.dart';
+
+Future<void> reloadTimeline(WidgetRef ref, TabSettings tabSettings) async {
+  if (!tabSettings.disableStreaming) {
+    ref.invalidate(webSocketChannelProvider(tabSettings.account));
+  }
+  ref.read(timelineCenterNotifierProvider(tabSettings).notifier).reset();
+  await Future.wait([
+    ref.refresh(timelineNotesAfterNoteNotifierProvider(tabSettings).future),
+    ref.refresh(timelineNotesNotifierProvider(tabSettings).future),
+    ref
+        .read(timelineLastViewedNoteIdNotifierProvider(tabSettings).notifier)
+        .saveFromDate(DateTime.now()),
+    if (!tabSettings.disableStreaming)
+      ref.read(timelineStreamNotifierProvider(tabSettings).notifier).connect(),
+  ]);
+}
