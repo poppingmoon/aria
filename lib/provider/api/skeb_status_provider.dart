42363d744715892db3d5f267973c8bf73c3ba9ed
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2025-01-11 13:58:45 +0900

    fix: use default user agent to get skeb status

--- lib/provider/api/skeb_status_provider.dart
+++ lib/provider/api/skeb_status_provider.dart
@@ -1,9 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../model/account.dart';
-import 'misskey_provider.dart';
+import '../dio_provider.dart';
+import '../user_agent_provider.dart';
 
 part 'skeb_status_provider.g.dart';
 
@@ -12,9 +14,18 @@ FutureOr<UsersGetSkebStatusResponse> skebStatus(
   Ref ref,
   Account account,
   String userId,
-) {
-  return ref
-      .watch(misskeyProvider(account))
-      .users
-      .getSkebStatus(UsersGetSkebStatusRequest(userId: userId));
+) async {
+  final userAgent = await ref.watch(userAgentProvider.future);
+  final response = await ref.watch(dioProvider).getUri<Map<String, dynamic>>(
+        Uri.https(
+          account.host,
+          'api/users/get-skeb-status',
+          {'userId': userId},
+        ),
+        options: userAgent != null
+            ? Options(headers: {'User-Agent': userAgent})
+            : null,
+      );
+
+  return UsersGetSkebStatusResponse.fromJson(response.data!);
 }
---
+++ lib/provider/user_agent_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_agent_provider.g.dart';
+
+@riverpod
+FutureOr<String?> userAgent(Ref ref) {
+  return switch (defaultTargetPlatform) {
+    TargetPlatform.android ||
+    TargetPlatform.iOS ||
+    TargetPlatform.macOS =>
+      InAppWebViewController.getDefaultUserAgent(),
+    _ => null,
+  };
+}
