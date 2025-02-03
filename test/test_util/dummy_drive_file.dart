0b4436be273d77df4de94db83fe5f41a6238ce9f
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2025-02-04 07:02:18 +0900

    test: add tests for NoteWidget

---
+++ test/test_util/dummy_drive_file.dart
@@ -0,0 +1,13 @@
+import 'package:misskey_dart/misskey_dart.dart';
+
+final dummyDriveFile = DriveFile(
+  id: '',
+  createdAt: DateTime(0),
+  name: '',
+  type: '',
+  md5: '',
+  size: 0,
+  isSensitive: false,
+  properties: const DriveFileProperties(),
+  url: '',
+);
