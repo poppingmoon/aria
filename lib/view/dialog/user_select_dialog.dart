90ee09aa20af7f32f6fa6768f3b37540821f5164
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2025-07-24 02:08:24 +0900

    feat: add search mode for user selection

--- lib/view/dialog/user_select_dialog.dart
+++ lib/view/dialog/user_select_dialog.dart
@@ -4,14 +4,20 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 
+import '../../constant/inifite_scroll_extent_threshold.dart';
+import '../../constant/max_content_width.dart';
 import '../../constant/shortcuts.dart';
 import '../../i18n/strings.g.dart';
 import '../../model/account.dart';
+import '../../model/pagination_state.dart';
 import '../../provider/api/i_notifier_provider.dart';
 import '../../provider/api/search_users_by_username_provider.dart';
+import '../../provider/api/search_users_notifier_provider.dart';
+import '../../provider/general_settings_notifier_provider.dart';
 import '../../provider/recently_used_users_notifier_provider.dart';
 import '../../util/punycode.dart';
 import '../widget/error_message.dart';
+import '../widget/pagination_bottom_widget.dart';
 import '../widget/user_preview.dart';
 import '../widget/user_sheet.dart';
 
@@ -46,136 +52,255 @@ class UserSelectDialog extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final i = ref.watch(iNotifierProvider(account)).valueOrNull;
-    final username = useState('');
-    final host = useState(localOnly ? '.' : '');
-    final users = username.value.isNotEmpty || host.value.isNotEmpty
-        ? ref.watch(
-            searchUsersByUsernameProvider(
-              account,
-              username.value.isNotEmpty ? username.value : null,
-              host.value.isNotEmpty ? toAscii(host.value) : null,
-            ),
-          )
-        : null;
-    final recentlyUsedUsers =
-        ref
-            .watch(recentlyUsedUsersNotifierProvider(account))
-            .valueOrNull
-            ?.where(
-              (user) =>
-                  (includeSelf || user.id != i?.id) &&
-                  (!localOnly || user.host == null),
-            )
-            .toList() ??
-        [];
+    final query = useState('');
+    final debouncedQuery =
+        useDebounced(query.value, const Duration(milliseconds: 500)) ?? '';
+    final host = useState('');
+    final debouncedHost =
+        useDebounced(host.value, const Duration(milliseconds: 500)) ?? '';
+    final searchByUsername = useState(
+      ref.watch(
+        generalSettingsNotifierProvider.select(
+          (settings) => settings.searchUsersByUsername,
+        ),
+      ),
+    );
+    final users = switch (searchByUsername.value) {
+      true when debouncedQuery.isNotEmpty || debouncedHost.isNotEmpty =>
+        ref.watch(
+          searchUsersByUsernameProvider(
+            account,
+            debouncedQuery.isNotEmpty ? debouncedQuery : null,
+            localOnly
+                ? '.'
+                : debouncedHost.isNotEmpty
+                ? toAscii(debouncedHost)
+                : null,
+          ).select<AsyncValue<PaginationState<UserDetailed>>>(
+            (users) => switch (users) {
+              AsyncValue(valueOrNull: final users?) => AsyncValue.data(
+                PaginationState(items: users, isLastLoaded: true),
+              ),
+              AsyncValue(:final error?, :final stackTrace?) => AsyncValue.error(
+                error,
+                stackTrace,
+              ),
+              _ => const AsyncValue.loading(),
+            },
+          ),
+        ),
+      false when debouncedQuery.isNotEmpty => ref.watch(
+        searchUsersNotifierProvider(
+          account,
+          debouncedQuery,
+          userOrigin: localOnly ? Origin.local : null,
+        ),
+      ),
+      _ => null,
+    };
+    final recentlyUsedUsers = ref.watch(
+      recentlyUsedUsersNotifierProvider(account),
+    );
+    final enableInfiniteScroll = ref.watch(
+      generalSettingsNotifierProvider.select(
+        (settings) => settings.enableInfiniteScroll,
+      ),
+    );
+    final controller = useScrollController();
+    final isAtBottom = useState(false);
+    useEffect(() {
+      void callback() {
+        if (controller.position.extentAfter < infiniteScrollExtentThreshold) {
+          if (!isAtBottom.value) {
+            if (!searchByUsername.value) {
+              ref
+                  .read(
+                    searchUsersNotifierProvider(
+                      account,
+                      query.value,
+                      userOrigin: localOnly ? Origin.local : null,
+                    ).notifier,
+                  )
+                  .loadMore();
+            }
+          }
+        }
+      }
+
+      if (enableInfiniteScroll) {
+        controller.addListener(callback);
+      }
+      return () => controller.removeListener(callback);
+    }, []);
+    useEffect(() {
+      Future(
+        () => ref
+            .read(generalSettingsNotifierProvider.notifier)
+            .setSearchUsersByUsername(searchByUsername.value),
+      );
+      return;
+    }, [searchByUsername.value]);
+    final theme = Theme.of(context);
 
     return AlertDialog(
-      title: Text(t.misskey.selectUser),
+      title: Row(
+        children: [
+          Expanded(child: Text(t.misskey.selectUser)),
+          if (searchByUsername.value)
+            IconButton(
+              tooltip: t.misskey.search,
+              onPressed: () => searchByUsername.value = false,
+              icon: const Icon(Icons.search),
+            )
+          else
+            IconButton(
+              tooltip: t.misskey.username,
+              onPressed: () => searchByUsername.value = true,
+              icon: const Icon(Icons.alternate_email),
+            ),
+        ],
+      ),
       content: SizedBox(
-        width: double.maxFinite,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Shortcuts(
-              shortcuts: disablingTextShortcuts,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: t.misskey.username,
-                        prefixText: '@',
-                        enabledBorder: Theme.of(
-                          context,
-                        ).inputDecorationTheme.border,
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                      ),
-                      onChanged: (value) => username.value = value,
-                      keyboardType: TextInputType.url,
-                      textInputAction: TextInputAction.next,
-                      onTapOutside: (_) => primaryFocus?.unfocus(),
-                    ),
-                  ),
-                  if (!localOnly) ...[
-                    const SizedBox(width: 8.0),
+        width: maxContentWidth,
+        child: CustomScrollView(
+          controller: controller,
+          slivers: [
+            PinnedHeaderSliver(
+              child: Shortcuts(
+                shortcuts: disablingTextShortcuts,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
                     Expanded(
                       child: TextField(
                         decoration: InputDecoration(
-                          labelText: t.misskey.host,
-                          prefixText: '@',
-                          enabledBorder: Theme.of(
-                            context,
-                          ).inputDecorationTheme.border,
+                          labelText: searchByUsername.value
+                              ? t.misskey.username
+                              : t.misskey.search,
+                          prefixText: searchByUsername.value ? '@' : null,
+                          prefixIcon: !searchByUsername.value
+                              ? const Icon(Icons.search)
+                              : null,
+                          enabledBorder: theme.inputDecorationTheme.border,
                           floatingLabelBehavior: FloatingLabelBehavior.always,
                         ),
-                        onChanged: (value) => host.value = value,
+                        onChanged: (value) => query.value = value,
                         keyboardType: TextInputType.url,
-                        textInputAction: TextInputAction.done,
+                        textInputAction: TextInputAction.next,
                         onTapOutside: (_) => primaryFocus?.unfocus(),
                       ),
                     ),
+                    if (searchByUsername.value && !localOnly) ...[
+                      const SizedBox(width: 8.0),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: t.misskey.host,
+                            prefixText: '@',
+                            enabledBorder: theme.inputDecorationTheme.border,
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                          ),
+                          onChanged: (value) => host.value = value,
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                          onTapOutside: (_) => primaryFocus?.unfocus(),
+                        ),
+                      ),
+                    ],
                   ],
-                ],
+                ),
               ),
             ),
-            Expanded(
-              child: switch (users) {
-                null =>
-                  recentlyUsedUsers.isNotEmpty
-                      ? ListView.separated(
-                          itemBuilder: (context, index) => UserPreview(
-                            account: account,
-                            user: recentlyUsedUsers[index],
-                            onTap: () => context.pop(recentlyUsedUsers[index]),
-                            onLongPress: () => showUserSheet(
-                              context: context,
-                              account: account,
-                              userId: recentlyUsedUsers[index].id,
-                            ),
-                          ),
-                          separatorBuilder: (_, _) =>
-                              const Divider(height: 0.0),
-                          itemCount: recentlyUsedUsers.length,
+            if (users == null)
+              switch (recentlyUsedUsers) {
+                AsyncValue(valueOrNull: final recentlyUsedUsers?) => Builder(
+                  builder: (context) {
+                    final users = recentlyUsedUsers
+                        .where(
+                          (user) =>
+                              (includeSelf || user.id != i?.id) &&
+                              (!localOnly || user.host == null),
                         )
-                      : Center(child: Text(t.misskey.noUsers)),
-                AsyncValue(valueOrNull: final users?) =>
-                  users.isEmpty
-                      ? Center(child: Text(t.misskey.nothing))
-                      : ListView.separated(
-                          itemBuilder: (context, index) =>
-                              includeSelf || users[index].id != i?.id
-                              ? UserPreview(
-                                  account: account,
-                                  user: users[index],
-                                  onTap: () {
-                                    ref
-                                        .read(
-                                          recentlyUsedUsersNotifierProvider(
-                                            account,
-                                          ).notifier,
-                                        )
-                                        .add(users[index]);
-                                    context.pop(users[index]);
-                                  },
-                                  onLongPress: () => showUserSheet(
-                                    context: context,
-                                    account: account,
-                                    userId: users[index].id,
-                                  ),
-                                )
-                              : const SizedBox.shrink(),
-                          separatorBuilder: (_, _) =>
-                              const Divider(height: 0.0),
-                          itemCount: users.length,
+                        .toList();
+
+                    return SliverList.separated(
+                      itemBuilder: (context, index) => UserPreview(
+                        account: account,
+                        user: users[index],
+                        onTap: () => context.pop(users[index]),
+                        onLongPress: () => showUserSheet(
+                          context: context,
+                          account: account,
+                          userId: users[index].id,
                         ),
-                AsyncValue(:final error?, :final stackTrace) => ErrorMessage(
-                  error: error,
-                  stackTrace: stackTrace,
+                      ),
+                      separatorBuilder: (_, _) => const Divider(height: 0.0),
+                      itemCount: users.length,
+                    );
+                  },
                 ),
-                _ => const Center(child: CircularProgressIndicator()),
+                AsyncValue(:final error?, :final stackTrace) =>
+                  SliverToBoxAdapter(
+                    child: ErrorMessage(error: error, stackTrace: stackTrace),
+                  ),
+                _ => const SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              }
+            else
+              ...switch (users) {
+                AsyncValue(valueOrNull: final value?) => [
+                  if (value.items
+                          .where((user) => includeSelf || user.id != i?.id)
+                          .toList()
+                      case final users when users.isNotEmpty)
+                    SliverList.separated(
+                      itemBuilder: (context, index) => UserPreview(
+                        account: account,
+                        user: users[index],
+                        onTap: () => context.pop(users[index]),
+                        onLongPress: () => showUserSheet(
+                          context: context,
+                          account: account,
+                          userId: users[index].id,
+                        ),
+                      ),
+                      separatorBuilder: (_, _) => const Divider(height: 0.0),
+                      itemCount: users.length,
+                    )
+                  else
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(child: Text(t.misskey.noUsers)),
+                    ),
+                  if (!value.isLastLoaded && !searchByUsername.value)
+                    SliverToBoxAdapter(
+                      child: PaginationBottomWidget(
+                        paginationState: users,
+                        loadMore: () => ref.read(
+                          searchUsersNotifierProvider(
+                            account,
+                            debouncedQuery,
+                            userOrigin: localOnly ? Origin.local : null,
+                          ),
+                        ),
+                        height: 40.0,
+                      ),
+                    ),
+                ],
+                AsyncValue(:final error?, :final stackTrace) => [
+                  SliverToBoxAdapter(
+                    child: ErrorMessage(error: error, stackTrace: stackTrace),
+                  ),
+                ],
+                _ => [
+                  const SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Center(child: CircularProgressIndicator()),
+                  ),
+                ],
               },
-            ),
           ],
         ),
       ),
