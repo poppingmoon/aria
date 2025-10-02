39d7bb56e5d64ff8c5013ad5a81ef192e7b60126
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2025-10-03 00:57:23 +0900

    feat: support decorations below avatar (#747)

--- lib/view/widget/cat_avatar.dart
+++ lib/view/widget/cat_avatar.dart
@@ -3,11 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 
 import '../../model/account.dart';
-import '../../provider/general_settings_notifier_provider.dart';
 import 'avatar_decorations.dart';
 import 'cat_ear.dart';
 import 'image_widget.dart';
@@ -26,17 +24,18 @@ final _catEarWiggleTween = TweenSequence([
   TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.65624379), weight: 25),
 ]);
 
-class CatAvatar extends HookConsumerWidget {
+class CatAvatar extends HookWidget {
   const CatAvatar({
     super.key,
     required this.account,
     required this.url,
     required this.blurHash,
     required this.catEarColor,
-    required this.showAvatarDecorations,
     required this.decorations,
+    required this.decorationsBelowAvatar,
     required this.showOnlineStatus,
     required this.onlineStatus,
+    required this.enableHapticFeedback,
     required this.size,
     required this.borderRadius,
     required this.onTap,
@@ -46,21 +45,17 @@ class CatAvatar extends HookConsumerWidget {
   final Uri? url;
   final String? blurHash;
   final Color catEarColor;
-  final bool showAvatarDecorations;
-  final List<UserAvatarDecoration> decorations;
+  final List<UserAvatarDecoration>? decorations;
+  final List<UserAvatarDecoration>? decorationsBelowAvatar;
   final bool showOnlineStatus;
   final OnlineStatus? onlineStatus;
+  final bool enableHapticFeedback;
   final double size;
   final BorderRadius borderRadius;
   final void Function()? onTap;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final enableHapticFeedback = ref.watch(
-      generalSettingsNotifierProvider.select(
-        (settings) => settings.enableHapticFeedback,
-      ),
-    );
+  Widget build(BuildContext context) {
     final controller = useAnimationController(
       duration: const Duration(seconds: 1),
     );
@@ -111,6 +106,12 @@ class CatAvatar extends HookConsumerWidget {
         child: Stack(
           alignment: Alignment.center,
           children: [
+            if (decorationsBelowAvatar case final decorations?)
+              AvatarDecorations(
+                account: account,
+                decorations: decorations,
+                size: size,
+              ),
             Transform.translate(
               offset: Offset(-size * 0.25, 0.0),
               child: CatEar(
@@ -138,7 +139,7 @@ class CatAvatar extends HookConsumerWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            if (showAvatarDecorations)
+            if (decorations case final decorations?)
               AvatarDecorations(
                 account: account,
                 decorations: decorations,
