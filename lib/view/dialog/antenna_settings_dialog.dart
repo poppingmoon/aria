ea95ab29266eb0b1bf3ee82eeb244120546197e9
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2025-10-05 01:01:19 +0900

    refactor: use LineSplitter to split lines

--- lib/view/dialog/antenna_settings_dialog.dart
+++ lib/view/dialog/antenna_settings_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -262,16 +264,23 @@ class AntennaSettingsDialog extends HookConsumerWidget {
         ElevatedButton(
           onPressed: () => context.pop(
             settings.value.copyWith(
-              keywords: keywordsController.text
-                  .trim()
-                  .split('\n')
-                  .map((e) => e.trim().split(' '))
+              keywords: LineSplitter.split(keywordsController.text.trim())
+                  .map(
+                    (line) => line
+                        .split(' ')
+                        .map((keyword) => keyword.trim())
+                        .toList(),
+                  )
                   .toList(),
-              excludeKeywords: excludeKeywordsController.text
-                  .trim()
-                  .split('\n')
-                  .map((e) => e.trim().split(' '))
-                  .toList(),
+              excludeKeywords:
+                  LineSplitter.split(excludeKeywordsController.text.trim())
+                      .map(
+                        (line) => line
+                            .split(' ')
+                            .map((keyword) => keyword.trim())
+                            .toList(),
+                      )
+                      .toList(),
             ),
           ),
           child: Text(t.misskey.save),
