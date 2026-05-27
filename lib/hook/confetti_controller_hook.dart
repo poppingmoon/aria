823c32db45bb79b3bd811a318154470a907d9a24
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2026-05-28 07:15:29 +0900

    fix: generate confetti from random positions (#919)

--- lib/hook/confetti_controller_hook.dart
+++ lib/hook/confetti_controller_hook.dart
@@ -4,18 +4,27 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 ConfettiController useConfettiController({
   Duration duration = const Duration(seconds: 30),
+  void Function(ParticleStats stats)? particleStatsCallback,
   List<Object?>? keys,
 }) {
-  return use(_ConfettiControllerHook(duration: duration, keys: keys));
+  return use(
+    _ConfettiControllerHook(
+      duration: duration,
+      particleStatsCallback: particleStatsCallback,
+      keys: keys,
+    ),
+  );
 }
 
 class _ConfettiControllerHook extends Hook<ConfettiController> {
   const _ConfettiControllerHook({
     this.duration = const Duration(seconds: 30),
+    this.particleStatsCallback,
     super.keys,
   });
 
   final Duration duration;
+  final void Function(ParticleStats stats)? particleStatsCallback;
 
   @override
   HookState<ConfettiController, Hook<ConfettiController>> createState() =>
@@ -24,10 +33,13 @@ class _ConfettiControllerHook extends Hook<ConfettiController> {
 
 class _ConfettiControllerHookState
     extends HookState<ConfettiController, _ConfettiControllerHook> {
-  late final controller = ConfettiController(duration: hook.duration);
+  late final controller = ConfettiController(
+    duration: hook.duration,
+    particleStatsCallback: hook.particleStatsCallback,
+  );
 
   @override
-  ConfettiController build(BuildContext context) => controller..play();
+  ConfettiController build(BuildContext context) => controller;
 
   @override
   void dispose() => controller.dispose();
--- lib/view/page/user/user_page.dart
+++ lib/view/page/user/user_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dart/misskey_dart.dart';
@@ -8,6 +13,7 @@ import '../../../hook/confetti_controller_hook.dart';
 import '../../../i18n/strings.g.dart';
 import '../../../model/account.dart';
 import '../../../provider/api/user_notifier_provider.dart';
+import '../../../provider/general_settings_notifier_provider.dart';
 import '../../widget/user_sheet.dart';
 import '../../widget/username_widget.dart';
 import 'user_clips.dart';
@@ -61,56 +67,56 @@ class UserPage extends HookConsumerWidget {
       _ => null,
     };
 
-    return DefaultTabController(
-      length: userId != null
-          ? 3 + (isLocal ? 6 : 0) + (isReactionsVisibile ? 1 : 0)
-          : 1,
-      child: Scaffold(
-        appBar: AppBar(
-          title: user != null
-              ? UsernameWidget(account: account, user: user)
-              : null,
-          bottom: user != null
-              ? TabBar(
-                  tabs: [
-                    Tab(text: t.misskey.overview),
-                    Tab(text: t.misskey.notes),
-                    if (isLocal) Tab(text: t.misskey.featured),
-                    Tab(text: t.misskey.files),
-                    if (isReactionsVisibile) Tab(text: t.misskey.reactions),
-                    if (isLocal) ...[
-                      Tab(text: t.misskey.clips),
-                      Tab(text: t.misskey.lists),
-                      Tab(text: t.misskey.pages),
-                      const Tab(text: 'Play'),
-                      Tab(text: t.misskey.gallery),
-                    ],
-                  ],
-                  isScrollable: isLocal,
-                  tabAlignment: isLocal ? TabAlignment.center : null,
-                )
-              : null,
-          actions: [
-            if (user is MeDetailed)
-              IconButton(
-                onPressed: () => context.push('/$account/qr'),
-                icon: const Icon(Icons.qr_code_rounded),
-              ),
-            if (userId != null)
-              IconButton(
-                onPressed: () => showUserSheet(
-                  context: context,
-                  account: account,
-                  userId: userId,
-                  disableHeader: true,
-                ),
-                icon: const Icon(Icons.more_vert),
-              ),
-          ],
-        ),
-        body: Stack(
-          children: [
-            TabBarView(
+    return Stack(
+      children: [
+        DefaultTabController(
+          length: userId != null
+              ? 3 + (isLocal ? 6 : 0) + (isReactionsVisibile ? 1 : 0)
+              : 1,
+          child: Scaffold(
+            appBar: AppBar(
+              title: user != null
+                  ? UsernameWidget(account: account, user: user)
+                  : null,
+              bottom: user != null
+                  ? TabBar(
+                      tabs: [
+                        Tab(text: t.misskey.overview),
+                        Tab(text: t.misskey.notes),
+                        if (isLocal) Tab(text: t.misskey.featured),
+                        Tab(text: t.misskey.files),
+                        if (isReactionsVisibile) Tab(text: t.misskey.reactions),
+                        if (isLocal) ...[
+                          Tab(text: t.misskey.clips),
+                          Tab(text: t.misskey.lists),
+                          Tab(text: t.misskey.pages),
+                          const Tab(text: 'Play'),
+                          Tab(text: t.misskey.gallery),
+                        ],
+                      ],
+                      isScrollable: isLocal,
+                      tabAlignment: isLocal ? TabAlignment.center : null,
+                    )
+                  : null,
+              actions: [
+                if (user is MeDetailed)
+                  IconButton(
+                    onPressed: () => context.push('/$account/qr'),
+                    icon: const Icon(Icons.qr_code_rounded),
+                  ),
+                if (userId != null)
+                  IconButton(
+                    onPressed: () => showUserSheet(
+                      context: context,
+                      account: account,
+                      userId: userId,
+                      disableHeader: true,
+                    ),
+                    icon: const Icon(Icons.more_vert),
+                  ),
+              ],
+            ),
+            body: TabBarView(
               children: [
                 UserHome(
                   account: account,
@@ -134,26 +140,73 @@ class UserPage extends HookConsumerWidget {
                 ],
               ],
             ),
-            if ((birthday?.day == now.day && birthday?.month == now.month) ||
-                (birthday?.day == 29 &&
-                    birthday?.month == 2 &&
-                    now.day == 1 &&
-                    now.month == 3 &&
-                    (now.year % 4 != 0 ||
-                        (now.year % 100 == 0 && now.year % 400 != 0))))
-              Align(
-                alignment: const Alignment(0.0, -0.5),
-                child: ConfettiWidget(
-                  confettiController: useConfettiController(
-                    duration: const Duration(seconds: 10),
-                  ),
-                  blastDirectionality: BlastDirectionality.explosive,
-                  numberOfParticles: 50,
-                  maxBlastForce: 80.0,
-                ),
-              ),
-          ],
+          ),
         ),
+        if ((birthday?.day == now.day && birthday?.month == now.month) ||
+            (birthday?.day == 29 &&
+                birthday?.month == 2 &&
+                now.day == 1 &&
+                now.month == 3 &&
+                (now.year % 4 != 0 ||
+                    (now.year % 100 == 0 && now.year % 400 != 0)))) ...[
+          const _Confetti(delay: Duration.zero),
+          ...Iterable.generate(10, (_) => const _Confetti()),
+          const _Confetti(delay: Duration(seconds: 5)),
+        ],
+      ],
+    );
+  }
+}
+
+class _Confetti extends HookConsumerWidget {
+  const _Confetti({this.delay});
+
+  final Duration? delay;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final enableHapticFeedback = ref.watch(
+      generalSettingsNotifierProvider.select(
+        (settings) => settings.enableHapticFeedback,
+      ),
+    );
+    final numberOfParticles = useRef(0);
+    final controller = useConfettiController(
+      duration: const Duration(seconds: 1),
+      particleStatsCallback: (stats) {
+        if (enableHapticFeedback &&
+            stats.numberOfParticles > numberOfParticles.value) {
+          HapticFeedback.lightImpact();
+        }
+        numberOfParticles.value = stats.numberOfParticles;
+      },
+    );
+    final alignment = useMemoized(
+      () => Alignment(switch (Random().nextDouble() - 0.5) {
+        final x when x < 0 => x * 1.5 - 0.2,
+        final x => x * 1.5 + 0.2,
+      }, pow(Random().nextDouble(), 1.5) * 2.0 - 1.0),
+    );
+    useEffect(() {
+      final delay =
+          this.delay ??
+          const Duration(seconds: 5) *
+              min(Random().nextDouble(), Random().nextDouble());
+      Future.delayed(delay, () => controller.play());
+      Future.delayed(
+        delay * 2 + const Duration(seconds: 1),
+        () => controller.play(),
+      );
+      return;
+    }, []);
+
+    return Align(
+      alignment: alignment,
+      child: ConfettiWidget(
+        confettiController: controller,
+        blastDirectionality: BlastDirectionality.explosive,
+        numberOfParticles: 50,
+        maxBlastForce: 80.0,
       ),
     );
   }
