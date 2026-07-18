3f220e44d7536eb8ccedb2add3afb533d72db860
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2026-07-18 21:23:55 +0900

    refactor: extract url parsing logic to function

--- lib/view/widget/url_widget.dart
+++ lib/view/widget/url_widget.dart
@@ -8,6 +8,84 @@ import '../../provider/misskey_colors_provider.dart';
 import '../../util/punycode.dart';
 import 'url_sheet.dart';
 
+String _decodeComponent(String encodedComponent) {
+  try {
+    return Uri.decodeComponent(encodedComponent);
+  } catch (_) {
+    return encodedComponent;
+  }
+}
+
+String _decodeQueryComponent(String encodedComponent) {
+  try {
+    return Uri.decodeQueryComponent(encodedComponent);
+  } catch (_) {
+    // TODO: Decode other encodings.
+    return encodedComponent;
+  }
+}
+
+typedef DisplayUrl = ({
+  String? scheme,
+  String host,
+  String? port,
+  String path,
+  String? query,
+  String? fragment,
+});
+
+DisplayUrl parseDisplayUrl(Uri url) {
+  return (
+    scheme: url.hasScheme ? '${url.scheme.breakAll}://' : null,
+    host: toUnicode(url.host).breakAll,
+    port: url.hasPort ? ':${url.port.toString().breakAll}' : null,
+    path: _decodeComponent(url.path).breakAll,
+    query: url.hasQuery
+        ? '?${_decodeQueryComponent(url.query).breakAll}'
+        : null,
+    fragment: url.hasFragment
+        ? '#${_decodeQueryComponent(url.fragment).breakAll}'
+        : null,
+  );
+}
+
+TextSpan buildUrlSpan({
+  required DisplayUrl url,
+  TextSpan Function({String? text, TextStyle? style}) textSpanBuilder =
+      TextSpan.new,
+  Color? color,
+  double opacity = 1.0,
+}) {
+  return TextSpan(
+    children: [
+      if (url.scheme != null)
+        textSpanBuilder(
+          text: url.scheme,
+          style: TextStyle(color: color?.withValues(alpha: opacity * 0.5)),
+        ),
+      textSpanBuilder(
+        text: url.host,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      if (url.port != null) textSpanBuilder(text: url.port),
+      textSpanBuilder(
+        text: url.path,
+        style: TextStyle(color: color?.withValues(alpha: opacity * 0.8)),
+      ),
+      if (url.query != null)
+        textSpanBuilder(
+          text: url.query,
+          style: TextStyle(color: color?.withValues(alpha: opacity * 0.5)),
+        ),
+      if (url.fragment != null)
+        textSpanBuilder(
+          text: url.fragment,
+          style: const TextStyle(fontStyle: FontStyle.italic),
+        ),
+    ],
+  );
+}
+
 class UrlWidget extends HookWidget {
   const UrlWidget({
     required this.url,
@@ -33,57 +111,16 @@ class UrlWidget extends HookWidget {
   final TextScaler? textScaler;
   final int? maxLines;
 
-  String _decodeComponent(String encodedComponent) {
-    try {
-      return Uri.decodeComponent(encodedComponent);
-    } catch (_) {
-      return encodedComponent;
-    }
-  }
-
-  String _decodeQueryComponent(String encodedComponent) {
-    try {
-      return Uri.decodeQueryComponent(encodedComponent);
-    } catch (_) {
-      // TODO: Decode other encodings.
-      return encodedComponent;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    final url = useMemoized(() => Uri.tryParse(this.url), [this.url]);
+    final url = useMemoized(
+      () => switch (Uri.tryParse(this.url)) {
+        final url? => parseDisplayUrl(url),
+        _ => null,
+      },
+      [this.url],
+    );
     final style = DefaultTextStyle.of(context).style.merge(this.style);
-    if (url == null) {
-      return InkWell(
-        onTap: onTap,
-        onLongPress: () => showModalBottomSheet<void>(
-          context: context,
-          builder: (context) => UrlSheet(url: this.url),
-        ),
-        child: Text(
-          this.url,
-          style: style.apply(
-            fontSizeFactor: scale,
-            color: style.color?.withValues(alpha: opacity),
-          ),
-          textAlign: align,
-          overflow: overflow,
-          textScaler: textScaler,
-          maxLines: maxLines,
-          semanticsLabel: this.url,
-        ),
-      );
-    }
-
-    final scheme = url.scheme;
-    final host = useMemoized(() => toUnicode(url.host), [url.host]);
-    final port = url.port;
-    final path = useMemoized(() => _decodeComponent(url.path), [url.path]);
-    final query = useMemoized(() => _decodeQueryComponent(url.query), [
-      url.query,
-    ]);
-    final fragment = useMemoized(() => _decodeComponent(url.fragment));
 
     return InkWell(
       onTap: onTap,
@@ -94,36 +131,10 @@ class UrlWidget extends HookWidget {
       child: Text.rich(
         TextSpan(
           children: [
-            if (scheme.isNotEmpty)
-              TextSpan(
-                text: '$scheme://'.breakAll,
-                style: TextStyle(
-                  color: style.color?.withValues(alpha: opacity * 0.5),
-                ),
-              ),
-            TextSpan(
-              text: host.breakAll,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            if (url.hasPort) TextSpan(text: ':$port'.breakAll),
-            TextSpan(
-              text: path.breakAll,
-              style: TextStyle(
-                color: style.color?.withValues(alpha: opacity * 0.8),
-              ),
-            ),
-            if (query.isNotEmpty)
-              TextSpan(
-                text: '?$query'.breakAll,
-                style: TextStyle(
-                  color: style.color?.withValues(alpha: opacity * 0.5),
-                ),
-              ),
-            if (fragment.isNotEmpty)
-              TextSpan(
-                text: '#$fragment'.breakAll,
-                style: const TextStyle(fontStyle: FontStyle.italic),
-              ),
+            if (url != null)
+              buildUrlSpan(url: url, color: style.color)
+            else
+              TextSpan(text: this.url),
             WidgetSpan(
               child: Icon(
                 Icons.open_in_new,
@@ -145,7 +156,7 @@ class UrlWidget extends HookWidget {
         overflow: overflow,
         textScaler: textScaler,
         maxLines: maxLines,
-        semanticsLabel: url.toString(),
+        semanticsLabel: this.url,
       ),
     );
   }
@@ -162,7 +173,7 @@ class _VerifiedIcon extends ConsumerWidget {
         Icons.check_circle_outline,
         color: ref.watch(
           misskeyColorsProvider(
-            Theme.of(context).brightness,
+            Theme.brightnessOf(context),
           ).select((colors) => colors.success),
         ),
         size: DefaultTextStyle.of(context).style.fontSize,
