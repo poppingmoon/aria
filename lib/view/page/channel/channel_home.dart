adb7f6f385ecd13072fa43df966da99c5f03b81f
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2026-08-20 21:48:27 +0900

    refactor: replace conditional with if

--- lib/view/page/channel/channel_home.dart
+++ lib/view/page/channel/channel_home.dart
@@ -84,39 +84,40 @@ class ChannelHome extends ConsumerWidget {
                           spacing: 8.0,
                           children: [
                             if (channel.isFollowing case final isFollowing?)
-                              isFollowing
-                                  ? ElevatedButton(
-                                      onPressed: () => futureWithDialog(
-                                        context,
-                                        ref
-                                            .read(
-                                              channelNotifierProvider(
-                                                account,
-                                                channelId,
-                                              ).notifier,
-                                            )
-                                            .unfollow(),
-                                      ),
-                                      child: Text(t.misskey.unfollow),
-                                    )
-                                  : ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        foregroundColor: colors.accent,
-                                        backgroundColor: colors.panel,
-                                      ),
-                                      onPressed: () => futureWithDialog(
-                                        context,
-                                        ref
-                                            .read(
-                                              channelNotifierProvider(
-                                                account,
-                                                channelId,
-                                              ).notifier,
-                                            )
-                                            .follow(),
-                                      ),
-                                      child: Text(t.misskey.follow),
-                                    ),
+                              if (isFollowing)
+                                ElevatedButton(
+                                  onPressed: () => futureWithDialog(
+                                    context,
+                                    ref
+                                        .read(
+                                          channelNotifierProvider(
+                                            account,
+                                            channelId,
+                                          ).notifier,
+                                        )
+                                        .unfollow(),
+                                  ),
+                                  child: Text(t.misskey.unfollow),
+                                )
+                              else
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: colors.accent,
+                                    backgroundColor: colors.panel,
+                                  ),
+                                  onPressed: () => futureWithDialog(
+                                    context,
+                                    ref
+                                        .read(
+                                          channelNotifierProvider(
+                                            account,
+                                            channelId,
+                                          ).notifier,
+                                        )
+                                        .follow(),
+                                  ),
+                                  child: Text(t.misskey.follow),
+                                ),
                             if (channel.isMuting ?? false)
                               DecoratedBox(
                                 decoration: BoxDecoration(
